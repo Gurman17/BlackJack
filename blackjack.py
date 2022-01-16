b835ef6a1d581b724f02c1f3862eae3d142e20c9
Gurman17 <chauhangurman@gmail.com>
2022-01-15 19:51:32 -0800

    Added blackjack.py

---
+++ blackjack.py
@@ -0,0 +1,20 @@
+
+# deck of cards / player dealer hand
+# deal the cards function
+# Count total of each hand
+# check for winner
+# Game loop
+
+import random
+
+class Card:
+    def __init__(self):
+        suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+        values = [i for i in range(1,13)]
+
+class Deck:
+    def __init__(self):
+        pass
+
+    def calculate_hand(self):
+        pass
