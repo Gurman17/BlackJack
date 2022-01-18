6e95e0109162454383753ef093e5dd5a0ad4b21f
Gurman17 <chauhangurman@gmail.com>
2022-01-18 10:14:55 -0800

    Updated classes. Added bet & hit functions

--- blackjack.py
+++ blackjack.py
@@ -1,20 +1,101 @@
 
-# deck of cards / player dealer hand
-# deal the cards function
-# Count total of each hand
-# check for winner
-# Game loop
+# face cards (jack, queen, king) count as 10
+# aces count as 1 or 11 (whichever is more favorable for the player)
 
+
+from cgi import test
 import random
 
-class Card:
-    def __init__(self):
-        suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
-        values = [i for i in range(1,13)]
+suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '8', '9', '10', 'Jack', 'Queen', 'King']
+values = {'Ace': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 
+          'Jack': 10, 'Queen': 10, 'King': 10}
 
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+    
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
+
+    
 class Deck:
     def __init__(self):
-        pass
+        self.deck = []
+        for rank in ranks:
+            for suit in suits:
+                self.deck.append(Card(suit, rank))
 
-    def calculate_hand(self):
-        pass
+    def __str__(self) -> str:
+        deck_comb = ''
+        for card in self.deck:
+            deck_comb += '\n ' + card.__str__()
+        return f'The deck has: {deck_comb}'
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        card = self.deck.pop()
+        return card
+
+class Hand:
+    def __init__(self):
+        self.cards =[]
+        self.value = 0
+        self.aces = 0
+    
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1
+    
+    def adjust_aces(self):
+        while (self.value > 21) and (self.aces > 0):
+            self.value -= 10
+            self.aces -= 1
+
+class Chips:
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+    
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('That is not a valid input. Enter an integer value.')
+        else:
+            if chips.bet > chips.total:
+                print(f"Sorry, your bet can't exceed {chips.total}")
+
+def hit(deck, hand):
+    hand.add_card(deck.deal())
+    hand.adjust_aces()
+
+
+
+
+
+
+test_deck = Deck()
+test_deck.shuffle()
+test_player = Hand()
+test_player.add_card(test_deck.deal())
+test_player.add_card(test_deck.deal())
+
+for card in test_player.cards:
+    print(card)
+
+print(test_player.value)
