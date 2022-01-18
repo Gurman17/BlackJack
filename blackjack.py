9ced72ad36cb8ac37d77b51312095a06c4f1889c
Gurman17 <chauhangurman@gmail.com>
2022-01-18 10:24:16 -0800

    added comments

--- blackjack.py
+++ blackjack.py
@@ -3,7 +3,6 @@
 # aces count as 1 or 11 (whichever is more favorable for the player)
 
 
-from cgi import test
 import random
 
 suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
@@ -11,7 +10,9 @@ ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '8', '9', '10', 'Jack', 'Quee
 values = {'Ace': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 
           'Jack': 10, 'Queen': 10, 'King': 10}
 
-class Card:
+
+# Create individual card 
+class Card:     
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
@@ -20,7 +21,7 @@ class Card:
     def __str__(self):
         return f'{self.rank} of {self.suit}'
 
-    
+# Create 52 card deck    
 class Deck:
     def __init__(self):
         self.deck = []
@@ -28,11 +29,12 @@ class Deck:
             for suit in suits:
                 self.deck.append(Card(suit, rank))
 
+    # Prints out current deck
     def __str__(self) -> str:
         deck_comb = ''
         for card in self.deck:
             deck_comb += '\n ' + card.__str__()
-        return f'The deck has: {deck_comb}'
+        return f'Current deck: {deck_comb}'
 
     def shuffle(self):
         random.shuffle(self.deck)
@@ -41,6 +43,8 @@ class Deck:
         card = self.deck.pop()
         return card
 
+
+# initalize hand
 class Hand:
     def __init__(self):
         self.cards =[]
@@ -53,11 +57,13 @@ class Hand:
         if card.rank == 'Ace':
             self.aces += 1
     
+    # adjust aces in players favor
     def adjust_aces(self):
         while (self.value > 21) and (self.aces > 0):
             self.value -= 10
             self.aces -= 1
 
+# class for chip betting, player starts with 100 chips
 class Chips:
     def __init__(self):
         self.total = 100
@@ -69,7 +75,7 @@ class Chips:
     def lose_bet(self):
         self.total -= self.bet
 
-
+# take user bet, loops until user enters valid bet (int)
 def take_bet(chips):
     while True:
         try:
@@ -82,13 +88,13 @@ def take_bet(chips):
 
 def hit(deck, hand):
     hand.add_card(deck.deal())
-    hand.adjust_aces()
+    hand.adjust_aces()  # Adjust for aces after each deal
 
 
 
 
 
-
+# Testing
 test_deck = Deck()
 test_deck.shuffle()
 test_player = Hand()
